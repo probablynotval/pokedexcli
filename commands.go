b86109212475465d67d3cb313063ca29308ec5c4
probablynotval <162844653+probablynotval@users.noreply.github.com>
2024-10-02 14:14:45 +0000

    feat: automatic handling of help commands

--- commands.go
+++ commands.go
@@ -6,7 +6,13 @@ import (
 )
 
 func commandHelp() error {
-	fmt.Printf("\nUsage:\nhelp: Displays this help message\nexit: Exits the program")
+	fmt.Println()
+	fmt.Println("Usage:")
+	fmt.Println()
+	for _, command := range getCommands() {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	fmt.Println()
 	return nil
 }
 
